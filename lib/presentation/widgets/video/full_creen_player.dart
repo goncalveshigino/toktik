545cf917fa8d6591ad947760ca2de16b252e33db
goncalveshigino <goncalveshigino20@gmail.com>
2023-04-23 15:38:28 +0100

    add grandient

--- lib/presentation/widgets/video/full_creen_player.dart
+++ lib/presentation/widgets/video/full_creen_player.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toktik/presentation/widgets/video/video_backgroun.dart';
 import 'package:video_player/video_player.dart';
 
 class FullScreenPlayer extends StatefulWidget {
@@ -43,17 +44,34 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
           return const Center(child: CircularProgressIndicator(strokeWidth: 2));
         }
 
-        return AspectRatio(
-          aspectRatio: controller.value.aspectRatio,
-          child: Stack(
-            children: [
-              Positioned(
-                  bottom: 50,
-                  left: 20,
-                  child: _VideoCaption(
-                    caption: widget.caption,
-                  ))
-            ],
+        return GestureDetector(
+          onTap: () {
+            if (controller.value.isPlaying) {
+              controller.pause();
+              return;
+            }
+
+            controller.play();
+          },
+          child: AspectRatio(
+            aspectRatio: controller.value.aspectRatio,
+            child: Stack(
+              children: [
+
+                VideoPlayer(controller),
+
+                VideoBackground(
+                  stops:const  [0.8, 1.0],
+                ),
+
+                Positioned(
+                    bottom: 50,
+                    left: 20,
+                    child: _VideoCaption(
+                      caption: widget.caption,
+                    ))
+              ],
+            ),
           ),
         );
       },
@@ -62,7 +80,6 @@ class _FullScreenPlayerState extends State<FullScreenPlayer> {
 }
 
 class _VideoCaption extends StatelessWidget {
-
   final String caption;
   const _VideoCaption({required this.caption});
 
